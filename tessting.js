29de5b0dfb88f510f728179cd21043df0b421384
iman2299 <imanhasannaser@gmail.com>
2021-04-20 04:12:20 +0300

    first commit

---
+++ tessting.js
@@ -0,0 +1,17 @@
+
+let  myName = "IMAN " ,
+site = "web page " ;
+document.write(" Hello My Name Is " + myName + " ...And This Is My First " + site );
+
+let name = prompt('What Is Your Name?', );
+
+alert(`You are ${name} `)
+
+let age = prompt('How old are you?', );
+
+alert( `You are ${name} ` + `You are ${age} years old!` );
+
+    
+
+
+
